ca1d21c5b83a1db15dba19e5e7ba30cf6cbb567f
redoxkali <alperarinc@hotmail.com>
2022-01-09 01:13:17 +0300

    Refactor Spring Projesi (Spring)

--- ws/src/main/java/com/socialmedia/ws/user/UserController.java
+++ ws/src/main/java/com/socialmedia/ws/user/UserController.java
@@ -1,22 +1,22 @@
 package com.socialmedia.ws.user;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.socialmedia.ws.shared.GenericResponse;
 
 
 @RestController
 public class UserController {
 
     @Autowired
-    UserRepository userRepository;
-
+    UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public void createUser(@RequestBody User user) {
-        userRepository.save(user);
-
+    public GenericResponse createUser(@RequestBody User user) {
+    	userService.save(user);
+    	return new GenericResponse();
+    	
     }
 }
